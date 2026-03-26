8d01e0e4d6a6af912e872bc794018df7b3a7a98b
Ayano <ayano@example.com>
2026-03-26 16:30:49 +0700

    fix: resolve sanity image type error in build

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -1,11 +1,9 @@
 import createImageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
-
 import { dataset, projectId } from '../env'
 
 // https://www.sanity.io/docs/image-url
 const builder = createImageUrlBuilder({ projectId, dataset })
 
-export const urlForImage = (source: SanityImageSource) => {
+export const urlForImage = (source: any) => {
   return builder.image(source)
 }
